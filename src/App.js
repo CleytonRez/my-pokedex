1e4e82f724d4918d26997371065b5cfeb45b3a57
Cleyton Eduardo Rezende <cleytonmgrezende@gmail.com>
2021-07-20 18:54:22 -0300

    create header and pokemons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className="Header">
+        <h1>Pokedex</h1>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Pok%C3%A9_Ball_icon.svg/2052px-Pok%C3%A9_Ball_icon.svg.png" alt="pokebola" width="100px" height="100px"></img>
+      </div>
+
+      <div className="Body">
+        <button type="button">Card View</button>
+        <button type="button">Table View</button>
+      </div>
+
+      <div className="Pokemon">
+        <img src="https://www.seekpng.com/png/full/99-991972_16-bit-bulbasaur-pixel-art-pokemon-bulbasaur.png" alt="bulbasaur" width="100px" height="100px"></img>
+        <img src="https://www.nicepng.com/png/full/223-2239852_ivysaur-ivysaur-pixel-art.png" alt="ivysaur" width="100px" height="100px"></img>
+        <img src="https://www.nicepng.com/png/full/168-1685243_venusaur-pokemon-pixel-art-venusaur.png" alt="venusaur" width="100px" height="100px"></img>
+        <img src="https://www.seekpng.com/png/full/99-991972_16-bit-bulbasaur-pixel-art-pokemon-bulbasaur.png" alt="bulbasaur" width="100px" height="100px"></img>
+        <img src="https://www.seekpng.com/png/full/99-991972_16-bit-bulbasaur-pixel-art-pokemon-bulbasaur.png" alt="bulbasaur" width="100px" height="100px"></img>
+
+      </div>
     </div>
   );
 }
