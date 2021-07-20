d4b90efcfb75ae70d82ac7b070a32d5f5faf3758
Cleyton Eduardo Rezende <cleytonmgrezende@gmail.com>
2021-07-20 19:20:55 -0300

    updates pokemons and header

--- src/App.js
+++ src/App.js
@@ -13,15 +13,16 @@ function App() {
         <button type="button">Table View</button>
       </div>
 
-      <div className="Pokemon">
-        <img src="https://www.seekpng.com/png/full/99-991972_16-bit-bulbasaur-pixel-art-pokemon-bulbasaur.png" alt="bulbasaur" width="100px" height="100px"></img>
-        <img src="https://www.nicepng.com/png/full/223-2239852_ivysaur-ivysaur-pixel-art.png" alt="ivysaur" width="100px" height="100px"></img>
-        <img src="https://www.nicepng.com/png/full/168-1685243_venusaur-pokemon-pixel-art-venusaur.png" alt="venusaur" width="100px" height="100px"></img>
-        <img src="https://www.pikpng.com/pngl/b/44-448591_charmander-pixel-art-pokemon-facile-clipart.png" alt="charmander" width="100px" height="100px"></img>
-        <img src="https://www.pikpng.com/pngl/b/197-1972676_charmeleon-charmeleon-pixel-art-grid-clipart.png" alt="charmeleon" width="100px" height="100px"></img>
-        <img src="https://www.seekpng.com/png/full/797-7970826_random-image-from-user-pokemon-charizard-pixel-art.png" alt="charizard" width="100px" height="100px"></img>
+      <div className="Pokemons">
+        <img src="https://www.seekpng.com/png/full/99-991972_16-bit-bulbasaur-pixel-art-pokemon-bulbasaur.png" alt="bulbasaur" width="100px" height="100px" className="pokemon"></img>
+        <img src="https://www.nicepng.com/png/full/223-2239852_ivysaur-ivysaur-pixel-art.png" alt="ivysaur" width="100px" height="100px" className="pokemon"></img>
+        <img src="https://www.nicepng.com/png/full/168-1685243_venusaur-pokemon-pixel-art-venusaur.png" alt="venusaur" width="100px" height="100px" className="pokemon"></img>
+        <img src="https://www.pikpng.com/pngl/b/44-448591_charmander-pixel-art-pokemon-facile-clipart.png" alt="charmander" width="100px" height="100px" className="pokemon"></img>
+        <img src="https://www.pikpng.com/pngl/b/197-1972676_charmeleon-charmeleon-pixel-art-grid-clipart.png" alt="charmeleon" width="100px" height="100px" className="pokemon"></img>
+        <img src="https://www.seekpng.com/png/full/797-7970826_random-image-from-user-pokemon-charizard-pixel-art.png" alt="charizard" width="100px" height="100px" className="pokemon"></img>
 
       </div>
+
     </div>
   );
 }
